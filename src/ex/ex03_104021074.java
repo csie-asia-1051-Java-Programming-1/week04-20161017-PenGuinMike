1b0cc819b3e3d166ffe70cfc03f1d0d535c53dd6
PenGuinMike <104021074@live.asia.edu.tw>
2016-10-17 14:56:52 +0800

    finish

---
+++ src/ex/ex03_104021074.java
@@ -0,0 +1,26 @@
+﻿package ex;
+/*
+ * Topic: 讓使用者輸入 一個正整數 n 然後顯示出下列圖形 (例 n = 4)
+	* 
+	** 
+	***
+	****
+ * Date: 2016/10/17
+ * Author: 104021074 楊永聖
+ */
+import java.util.*;
+public class ex03_104021074 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for (int x = 1 ; x <= n ; x++){
+			for(int y = 1; y<=x ; y++){
+				System.out.print("*");
+			}System.out.println();
+		}
+		
+	}
+
+}
---
+++ src/ex/ex04_104021074.java
@@ -0,0 +1,28 @@
+﻿package ex;
+/*
+ * Topic: 讓使用者輸入 一個正整數 n 及一個字元符號，然後顯示出下列圖形 (例 n = 4,字元符號= @)
+    @@@@
+    @@@  
+    @@  
+    @
+ * Date: 2016/10/17
+ * Author: 104021074 楊永聖
+ */
+import java.util.*;
+public class ex04_104021074 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int x =scn.nextInt();
+		char r = scn.next().charAt(0);
+		for (int a =x ; a> 0 ; a--){
+			for (int b = 1 ; b<= a; b++){
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
